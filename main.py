a3794481a83b95af277a63138ea54c6d37d1c74c
TugceOnder <tugceonder775@gmail.com>
2020-11-16 17:22:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ result =random.randint(1,100)
 names=['ali','yagmur','deniz']
 result1=names[random.randint(0,len(names)-1)]
 
-print(result)
-print(result1)
+print(result) # bir ile 100 arası sayı belirleme
+print(result1) # listeden birinin adını seçme
 
 #PİYANGO ÇEKİLİŞİNE ÖRNEK 
 liste=range(10)
